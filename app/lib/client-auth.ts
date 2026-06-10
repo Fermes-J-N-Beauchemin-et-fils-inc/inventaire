66d47071e166b84a327f02506b7f1ad504ed4b26
jimmy-razafindretsa <jimmyyoelrazafindretsa@gmail.com>
2026-06-10 08:21:36 -0400

    fix : CORS error fix

--- app/lib/client-auth.ts
+++ app/lib/client-auth.ts
@@ -1,6 +1,4 @@
 import { createAuthClient } from "better-auth/react"
-export const { signIn, signUp, useSession } = createAuthClient()
-export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
-})
+
+export const authClient = createAuthClient()
+export const { signIn, signUp, useSession } = authClient
