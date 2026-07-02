a0d47895bcf81a49818211d518f8f73b73ac557a
jimmy-razafindretsa <jimmyyoelrazafindretsa@gmail.com>
2026-07-02 15:29:03 -0400

    fix: add force-dynamic to groupements to fix build

--- app/laitier/groupements/page.tsx
+++ app/laitier/groupements/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import React from 'react';
 import { getMixBatches, getUnassignedGroups } from './actions';
 import GroupingsClient from './components/GroupingsClient';
